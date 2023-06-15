1227e05328ba25598549c7cf7950113995144b4d
AnthonyUtt <anthony@anthonyutt.dev>
2023-06-15 15:32:25 -0400

    basic mako setup

--- home/anthony/features/desktop/common/wayland/mako.nix
+++ home/anthony/features/desktop/common/wayland/mako.nix
@@ -1,5 +1,26 @@
+{ ... }: 
+let
+  theme = import ../../../../theme;
+in
 {
   services.mako = {
     enable = true;
+    actions = true;
+    anchor = "top-center";
+    defaultTimeout = 15;
+
+    # Formatting
+    backgroundColor = theme.colors.notifications.background;
+    textColor = theme.colors.notifications.foreground;
+    margin = "10";
+
+    # Border
+    borderColor = null;
+    borderRadius = 5;
+    borderSize = 0;
+
+    extraConfig = ''
+
+    '';
   };
 }
--- home/anthony/theme/default.nix
+++ home/anthony/theme/default.nix
@@ -1,18 +1,21 @@
 {
   colors = rec {
-    black = "665C54";
-    black-alt = "282828";
-    white = "DFBF8E";
-    gray = "778691";
-    gray-alt = "AFB7BD";
-    blue = "2C93D4";
-    blue-alt = "69AFDB";
-    red = "EA6962";
-    green = "A9B665";
-    yellow = "E78A4E";
-    magenta = "D3869B";
-    cyan = "89B482";
-    cyan-alt = "7DAEA3";
+    true-white = "FFFFFF";  #FFFFFF
+    true-black = "000000";  #000000
+
+    black = "665C54";  #665C54
+    black-alt = "282828";  #282828
+    white = "DFBF8E";  #DFBF8E
+    gray = "778691";  #778691
+    gray-alt = "AFB7BD";  #AFB7BD
+    blue = "2C93D4";  #2C93D4
+    blue-alt = "69AFDB";  #69AFDB
+    red = "EA6962";  #EA6962
+    green = "A9B665";  #A9B665
+    yellow = "E78A4E";  #E78A4E
+    magenta = "D3869B";  #D3869B
+    cyan = "89B482";  #89B482
+    cyan-alt = "7DAEA3";  #7DAEA3
 
 
     # Symbolic names
@@ -23,5 +26,14 @@
     focused-alt = blue-alt;
     inactive = gray;
     inactive-alt = gray-alt;
+
+    window-manager = {
+      focused = focused;
+    };
+
+    notifications = {
+      background = true-white;
+      foreground = gray;
+    };
   };
 }
