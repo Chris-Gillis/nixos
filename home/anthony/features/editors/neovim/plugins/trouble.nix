584f7f9090bc35261bb00be5f5898b06f7125df8
AnthonyUtt <anthony@anthonyutt.dev>
2023-06-22 15:34:49 -0400

    update trouble api calls

--- home/anthony/features/editors/neovim/plugins/trouble.nix
+++ home/anthony/features/editors/neovim/plugins/trouble.nix
@@ -5,7 +5,7 @@
     require("trouble").setup()
 
     vim.keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>xw', '<cmd>TroubleToggle lsp_workspace_diagnostics<cr>', { noremap = true, silent = true })
-    vim.keymap.set('n', '<leader>xd', '<cmd>TroubleToggle lsp_document_diagnostics<cr>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>', { noremap = true, silent = true })
   '';
 }
