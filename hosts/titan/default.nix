13126c47328f79511e2af752f69695006ab410f8
AnthonyUtt <anthony@anthonyutt.dev>
2024-01-30 09:31:43 -0500

    remove quiet boot

--- hosts/titan/default.nix
+++ hosts/titan/default.nix
@@ -10,7 +10,6 @@
     ../common/optional/workstation.nix
     ../common/optional/pipewire.nix
     ../common/optional/docker.nix
-    ../common/optional/quiet-boot.nix
     ../common/optional/greetd.nix
     ../common/optional/flatpak.nix
     ../common/optional/gamemode.nix
