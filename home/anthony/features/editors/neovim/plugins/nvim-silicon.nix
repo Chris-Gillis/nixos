9b2c7b5eacd4b2d2efb0f850caedfa004806a2db
AnthonyUtt <anthony@anthonyutt.dev>
2024-05-31 11:26:11 -0400

    feat: enable nvim-silicon

--- home/anthony/features/editors/neovim/plugins/nvim-silicon.nix
+++ home/anthony/features/editors/neovim/plugins/nvim-silicon.nix
@@ -14,14 +14,14 @@ in
   plugin = (pluginFromGit "michaelrommel/nvim-silicon" "main" "ab664633e5090809da7470288404fe5b8735a8c8");
   type = "lua";
   config = ''
-    -- require("silicon").setup({
-    --   font = "ComicShannsMono Nerd Font=34",
-    --   to_clipboard = true,
-    -- })
+    require("silicon").setup({
+      font = "ComicShannsMono Nerd Font=34",
+      to_clipboard = true,
+    })
 
-    -- local wk = require("which-key")
-    -- wk.register({
-    --  ["<leader>ss"] = { ":Silicon <CR>", "Screenshot selection (silicon)" }
-    -- }, { mode = "x" })
+    local wk = require("which-key")
+    wk.register({
+      ["<leader>ss"] = { ":Silicon <CR>", "Screenshot selection (silicon)" }
+    }, { mode = "x" })
   '';
 }
